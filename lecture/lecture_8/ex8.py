9424200d624ce8cb5ba1d2ca092a93b00e4e01b7
Austin Yan <austin899899@gmail.com>
2024-11-02 17:58:04 +0800

    Finishing HW4 Exercise 1, find the step size(h) that minimize error. log-log plot of relative error and step size.

--- lecture/lecture_8/ex8.py
+++ lecture/lecture_8/ex8.py
@@ -14,21 +14,35 @@ def cos(x):
 def central_difference_1(func, x, h):
     return ((func(x+h) - func(x))-(func(x) - func(x-h)))/h**2
 
-numerical_1 = central_difference_1(cos, x, h)
-diff_1 = abs(numerical_1 - true_ans)/true_ans
-print(f"h={h}, answer={numerical_1}, relative error={diff_1}")
+h1 = np.linspace(10e-10, np.pi/10, 100000, endpoint=True) # start from 10^-10 to pi/10, 100000 points
+numerical_1 = central_difference_1(cos, x, h1)
+log_h1 = np.log(h1)
+diff_1 = abs((numerical_1 - true_ans)/true_ans)
+log_diff_1 = np.log(diff_1)
+min_error_index = np.argmin(log_diff_1) # return the index of the minimum value
+optimal_h = h1[min_error_index]
+print(f"The step size that minimizes the relative error is approximately: {optimal_h}")
+
+plt.figure(figsize=(10, 5), layout='constrained')
+plt.plot(log_h1, log_diff_1, marker='o', label='central difference')
+plt.xlabel(r"$ln h$")
+plt.ylabel(r"$ln \varepsilon$")
+plt.legend(loc='best')
+plt.grid('True')
+plt.show()
+# print(f"h={h}, answer={numerical_1}, relative error={log_diff_1}")
 
 # eq 2
 
 def central_difference_2(func, x, h):
     return (func(x+h) + func(x-h) - 2*func(x))/h**2
 numerical_2 = central_difference_2(cos, x, h)
-diff_2 = abs(numerical_2 - true_ans)/true_ans
-print(f"h={h}, answer={numerical_2}, relative error={diff_2}")
+# diff_2 = abs(numerical_2 - true_ans)/true_ans
+# print(f"h={h}, answer={numerical_2}, relative error={diff_2}")
 
 
 
-# if I run the same program in different computer with same seed, the output  will be the same.
+# If I run the same program in different computer with same seed, the output  will be the same.
 rng_1 = random.default_rng(seed=1000)
 rng_2 = random.default_rng(seed=1937)
 N = 1.0e5
@@ -37,13 +51,15 @@ x = rng_1.uniform(low=-1, high=1, size=int(N))
 y = rng_2.uniform(low=-1, high=1, size=int(N))
 func = lambda x, y: x**2 + y**2
 
-inside_x = np.where(func(x, y) < 1)
-num_inside = len(inside_x[0])
-ratio = num_inside/N
-Area = 4*ratio
-relative_error = abs(Area - np.pi)/np.pi
-print(f"N={N}, Area={Area}, relative error={relative_error}")
-# log_N = np.log10(np.)
+# Monte Carlo integration
+# inside_x = np.where(func(x, y) < 1)
+# num_inside = len(inside_x[0])
+# ratio = num_inside/N
+# Area = 4*ratio
+# relative_error = abs(Area - np.pi)/np.pi
+# print(f"N={N}, Area={Area}, relative error={relative_error}")
+
+# log_N = np.log(np.)
 # log_relative_error = np.log10(relative_error)
 
 # plt.figure()
