a498d3bc0c4761405134349f8d6c6a948a56c8d3
Austin Yan <austin899899@gmail.com>
2024-11-03 15:51:40 +0800

    Modify few bugs in solve_lower and upper triangle, b[j] will return ndarray, but b[j,0] will return float. Add two more function, first one is decompose matrix into LU, second is solve linear system using LU technique.

--- lecture/lecture_9/linalg.py
+++ lecture/lecture_9/linalg.py
@@ -15,27 +15,51 @@ def solve_lower_triangular_matrix(L, b):
     for j in range(L.shape[1]):
         if L[j,j] == 0:
             break
-        x[0,j] = b[j] / L[j,j]
+        x[0,j] = b[j,0] / L[j,j] # float64
         for i in range(j, L.shape[0]):
-            b[i] -= L[i,j]*x[0,j]
-    return x.T
+            b[i,0] -= L[i,j]*x[0,j]
+    return x.T # return x as a column vector(Transpose)
 
 def solve_upper_triangular_matrix(U, b):
     x = np.zeros((1,b.shape[0]))
     for j in range(b.shape[0]-1, -1, -1):
         if U[j,j] == 0:
             break
-        x[0,j] = b[j] / U[j,j]
+        x[0,j] = b[j,0] / U[j,j]
         for i in range(0, j):
-            b[i] -= U[i,j]*x[0,j]
+            b[i,0] -= U[i,j]*x[0,j]
     return x.T
-    
+
+def LU_decomposition(A):
+    M = np.zeros(A.shape)
+    U = np.zeros(A.shape)
+    n = A.shape[0]
+    for k in range(n):
+        if A[k,k] == 0:
+            break
+        for i in range(k+1, n):
+            M[i,k] = A[i,k]/A[k,k]
+        for j in range(k+1, n):
+            for i in range(k+1, n):
+                A[i,j] -= M[i,k]*A[k,j]
+        for j in range(k, n):
+            U[k,j] = A[k,j]
+    L = M + np.eye(n)
+    return L, U
+
+def solve_lu(A, b):
+    L, U = LU_decomposition(A)
+    y = solve_lower_triangular_matrix(L, b)
+    x = solve_upper_triangular_matrix(U, y)
+    return x
 
 if __name__ == "__main__":
-    a=np.array(
-        [[1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [10, 11, 12]]
-    )
-    mat_print("Matrix a", a)
+    A_exercise4_1 = np.array([[1, 2, 2],
+                        [4, 6, 8],
+                        [4, 8, 10]])
+
+    b_exercise4_1 = np.array([[4],
+                            [6],
+                            [10]])
+    x = solve_lu(A_exercise4_1, b_exercise4_1)                          
+    print(x)
