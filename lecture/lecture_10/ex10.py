681ddf55f81042ae738e6e14ebb9a90f6bce75b8
Austin Yan <austin899899@gmail.com>
2024-11-04 23:42:51 +0800

    modify the file name into systematically naming.

--- lecture/lecture_10/ex10.py
+++ lecture/lecture_10/ex10.py
@@ -1,31 +1,27 @@
 import numpy as np
 from physics import update
 from scipy.constants import g
-import matplotlib.pyplot as plt
 import pandas as pd
-path = r"/home/austin/Documents/computer/numerical/lecture/lecture_10/data//" # Ubuntu path
+import os
 
 '''
-This file mainly write the output data (obtained by different method) into a csv file
+This file mainly write the output data (obtained by different method) into a csv file.
+when execute this file only need to change the method name, and the step size (dt)
 '''
+path = f"lecture/lecture_10/data//" # Ubuntu path
 
 print("g = ", g) # m/s^2
-
 
 angle = 60.0  # degree
 angle = angle * np.pi / 180 # degree to rad
 
-
 velocity = 30 # m/s
-
 
 velx = velocity * np.cos(angle)
 vely = velocity * np.sin(angle)
 
-
-dt = 0.5 # timestep to advance the solution in sec
+dt = 0.1 # timestep to advance the solution in sec
 time = 0.0 # initial time
-
 
 posx = 0.0
 posy = 0.0
@@ -36,10 +32,11 @@ vy0    = vely # for computing the analytical solution
 err_y  = 0.0
 
 
-method = 'euler' # select solver ('euler', 'rk2', or 'rk4')
+method = 'rk2' # select solver ('euler', 'rk2', or 'rk4')
+# Ensure the directory exists, otherwise auto create it
+os.makedirs(path, exist_ok=True)
 
-
-f = open(path+method+'_t=0.5_.csv', 'w')
+f = open(path+f'_t={dt}_{method}.csv', 'w')
 # csv file don't need space between the comma
 f.write('time,posx,posy,velx,vely,anal_y,err_y \n')
 f.write(f"{time:e},{posx:e},{posy:e},{velx:e},{vely:e},{anal_y:e},{err_y:e}\n")
@@ -55,6 +52,4 @@ while (posy >= 0.0):
 
 f.close()
 
-
 print("Done")
-
