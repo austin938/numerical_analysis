63da16195a83385c5432395cd30fdee3c5bdfcb5
Austin Yan <austin899899@gmail.com>
2024-11-04 23:48:34 +0800

    Manage directory and filename in systematic naming, use different color, marker and linestyle for different data.

--- lecture/lecture_10/ex10_plot.py
+++ lecture/lecture_10/ex10_plot.py
@@ -2,20 +2,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import glob
+"""
+This script reads CSV files containing projectile motion data, plots the data using matplotlib, 
+and saves the plot as a PNG file. It supports different numerical methods: Euler, RK2, and RK4.
+Only input needed is the method name.
+"""
 
 # Define the path to the CSV files
-path = r"/home/austin/Documents/computer/numerical/lecture/lecture_10/data//" # Ubuntu path
+method = 'rk2' # select solver ('euler', 'rk2', or 'rk4')
+path = f"lecture/lecture_10/data//" # Ubuntu path
 # Define the directory and filename for the plot
 directory = "lecture/lecture_10/plot"
-filename = 'Projectile_motion.png'
+filename = f'Projectile_motion_{method}.png'
 # Pattern to match all CSV files
-file_pattern = path + '*.csv' 
+file_pattern = path + f'*{method}.csv' 
 
 # Get a list of all CSV files in the directory
 csv_files = glob.glob(file_pattern)
 
 # Initialize a figure for plotting
 plt.figure(figsize=(10, 5))
+
+# Define the property cyclers for color, marker, and linestyle
+color_cycler = plt.gca()._get_lines.prop_cycler
+marker_cycler = iter(plt.Line2D.filled_markers)
+linestyle_cycler = iter(['-', '--', '-.', ':'])
 
 # Loop through the list of CSV files
 for file in csv_files:
@@ -25,24 +36,29 @@ for file in csv_files:
     # Extract the required columns
     posx = df['posx']
     posy = df['posy']
-    
+
+    # Generate a unique color marker andd linestyle for each file
+    color = next(color_cycler)['color']
+    marker = next(marker_cycler)
+    linestyle = next(linestyle_cycler)
+
     # Plot the data
     # file = /home/austin/Documents/computer/numerical/lecture/lecture_10/data/euler_t=0.1.csv, etc..., use split to get the method name
-    plt.plot(posx, posy, label=file.split('/')[-1].split('_')[1], marker='o')
+    plt.plot(posx, posy, label=file.split('/')[-1].split('_')[1], color=color,  linestyle=linestyle)
     # use t=0.1 as the reference to plot the analytical solution
     if file.split('/')[-1].split('_')[1] == 't=0.1':
         anal_y = df['anal_y']
-        plt.plot(posx, anal_y, label='analytical', marker='o')
+        plt.plot(posx, anal_y, label='analytical', color='b', linestyle=linestyle)
 
 # set the y axis positive
 plt.ylim(0, 50)
 plt.xlabel('x(m)')
 plt.ylabel('y(m)')
-plt.title('Projectile motion by euler method')
+plt.title(f'Projectile motion by {method} method')
 plt.legend(loc='best')
 plt.grid('True')
 
-# Ensure the directory exists, otherwise auto create it
+# Create directory for plot if it does not exist
 os.makedirs(directory, exist_ok=True)
 
 # Save the plot to the specified directory with the specified filename
